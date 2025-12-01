de85189da9c23b00bb9a8de36c9c99bc88375676
GizmekOrochi <valcurion369@example.com>
2025-12-01 13:12:47 +0100

    pour flo

--- src/include/config/Settings.hpp
+++ src/include/config/Settings.hpp
@@ -14,12 +14,12 @@ public:
     // Physics
     float baseSpeed        = 1.0f;   // basespeed
 
-    float maxSpeed        = 4.0f;   // vmax
+    float maxSpeed        = 3.0f;   // vmax
     float maxAcceleration = 0.1f;   // amax
     float deltaTime       = 1.0f;   // delta
 
     // Boids vision range
-    float visionAngleDeg = 360.0f;   // default: half circle
+    float visionAngleDeg = 180.0f;   // default: half circle
     float visionRange    = 100.0f;    // default: same as perceptionRadius
     
     // Boids perception
